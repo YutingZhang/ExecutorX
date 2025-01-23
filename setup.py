8788d21784f13bc1566359b895860ad2c3c7a55f
Yuting Zhang <yutingzh@example.com>
2025-01-22 21:43:20 -0500

    add requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-# Read the version from ExecutorX/__version__.py
+# Read the version from executorx/__version__.py
 version = {}
 with open(os.path.join("executorx", "__version__.py")) as fp:
     exec(fp.read(), version)
@@ -9,6 +9,11 @@ with open(os.path.join("executorx", "__version__.py")) as fp:
 # Read the content of the README.md file
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+# Read requirements from requirements.txt
+def parse_requirements(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
 
 setup(
     name="ExecutorX",
@@ -26,5 +31,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
-    install_requires=[],
+    install_requires=parse_requirements("requirements.txt"),  # Load dependencies dynamically
 )
