2fc580c066601f43428a8ce03a2d5317aec17d06
Yuting Zhang <yutingzh@example.com>
2025-01-31 11:19:00 -0500

    add var registry

--- examples/example_progressbar.py
+++ examples/example_progressbar.py
@@ -1,14 +1,20 @@
 from executorx.futures import ProcessPoolExecutor, ThreadPoolExecutor
-from executorx.addons import Progress
+from executorx.addons import Progress, VarRegistry
 import time
 
 
 def func(x):
     time.sleep(0.1)
-    return x * x
+    a = x * x * VarRegistry.get('base')
+    print(a)
+    return a
 
 def main():
-    executor = ProcessPoolExecutor(max_workers=4, addons=[Progress()])
+    executor = ProcessPoolExecutor(
+        max_workers=4, addons=[
+            Progress(), VarRegistry(base=100)
+        ]
+    )
     for i in range(100):
         executor.submit(func, i)
     executor.join()
--- executorx/addons/__init__.py
+++ executorx/addons/__init__.py
@@ -7,3 +7,4 @@ __author__ = ['Yuting Zhang']
 from .progress import *
 from .throttle import *
 from .worker_id import *
+from .var_registry import *
